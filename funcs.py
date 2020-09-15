e97e98a0fc0646d2247891bab62ecb13ea20ba4c
lancev.veigas3@gmail.com <lancev.veigas3@gmail.com>
2020-09-15 18:57:56 +0530

    modified workflow

--- funcs.py
+++ funcs.py
@@ -1,23 +1,38 @@
 import os
-from tqdm import tqdm
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver import ActionChains
-from selenium.webdriver.support import expected_conditions as EC   
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.select import Select
+import cv2
+import shutil
 import time as t
-import re
-from bs4 import BeautifulSoup
 import pandas as pd
 import urllib.request
 from PIL import Image
-import shutil
+from tqdm import tqdm
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC   
 
 ch_path = os.path.join(os.getcwd(), 'chromedriver.exe')
 dwn_path = os.path.join(os.getcwd(), 'temp')
 images_path = os.path.join(os.getcwd(), 'images')
+compressed_path = os.path.join(os.getcwd(), 'compressed')
+
+def get_images(link, type='order'):
+  clear_old()
+  if type=='order':
+    scrap_images(link)
+  elif type=='photos':
+    scrap_non_o2_images(link)
+  save_images()
+
+def crop_and_compress(image, dim):
+  base = os.path.basename(image)
+  __, ext = os.path.splitext(base)
+
+  if ext != '':
+    crop_image(image, dim)
+    compress_image(image)
 
 def initiate_driver():
   chromeOptions = webdriver.ChromeOptions()
@@ -36,7 +51,7 @@ def scrap_images(link):
 
   driver, wait = initiate_driver()
 
-  driver.minimize_window()
+  driver.maximize_window()
   driver.get(link)
 
   div_selector = r"div.sc-1s0saks-4.gkwyfN"
@@ -56,7 +71,6 @@ def scrap_images(link):
     element = div.find_elements_by_css_selector(img_selector)
 
     while (element == None) | (element == []) | (len(element)==0):
-      print('hit empty image')
       t.sleep(1)
       element = div.find_elements_by_css_selector(img_selector)
 
@@ -82,23 +96,20 @@ def save_images():
   names = file['Name'].tolist()
   links = file['link'].tolist()
 
-  for index, link in enumerate(tqdm(links)):
-    link = link.split('?')[0]
-    
-    ext = link[-3:]
-
-    urllib.request.urlretrieve(link, images_path + f"//{names[index]}.{ext}")
-
-def resize_image(image_path, size):
-  image = Image.open(image_path)
-
-  width, height = image.size
-  if (width==size[0] and height==size[1]):
-    return image
+  if 'Gallery Image' in names:
+    for index, link in enumerate(tqdm(links)):
+      link = link.split('?')[0]
+      base = os.path.basename(link)
+      __, ext = os.path.splitext(base)
+      
+      urllib.request.urlretrieve(link, images_path + f"//{index}.{ext}")
   else:
-    image = image.resize(size)
-    image.save(image_path)
-    return image
+    for index, link in enumerate(tqdm(links)):
+      link = link.split('?')[0]
+      base = os.path.basename(link)
+      __, ext = os.path.splitext(base)
+
+      urllib.request.urlretrieve(link, images_path + f"//{names[index]}.{ext}")
 
 def crop_image(image_path, dim):
   image = Image.open(image_path)
@@ -120,7 +131,7 @@ def crop_image(image_path, dim):
       return None
     else:
       new_image = image.resize(dim, Image.ANTIALIAS)
-      new_image.save(image_path, optimize=True, quality=80)
+      new_image.save(image_path)
     return None
 
   left = (width - new_width)/2
@@ -136,7 +147,7 @@ def crop_image(image_path, dim):
     return None
   else:
     new_image = new_image.resize(dim, Image.ANTIALIAS)
-    new_image.save(image_path, optimize=True, quality=80)
+    new_image.save(image_path)
 
   return None
 
@@ -147,6 +158,13 @@ def clear_old():
 
       if ext=='':
         if  'images' in __:
+          path = os.path.join(os.getcwd(), f"{l}")
+          try:
+            shutil.rmtree(path)
+            os.mkdir(path)
+          except Exception as e:
+            print(e)
+        elif 'compressed' in __:
           path = os.path.join(os.getcwd(), f"{l}")
           try:
             shutil.rmtree(path)
@@ -165,6 +183,63 @@ def clear_old():
           os.mkdir(path)
         except:
           pass
+
+def compress_image(image_path, quality=85):
+  f_name = os.path.basename(image_path)
+  f, _ = os.path.splitext(f_name)
+
+  image = cv2.imread(image_path)
+  
+  cv2.imwrite(compressed_path + f"//{f}.jpg", image, [int(cv2.IMWRITE_JPEG_QUALITY), quality])
+
+def scrap_non_o2_images(link):
+  if not (link.endswith('photos')):
+    link = link + '/photos'
+
+  driver, wait = initiate_driver()
+
+  driver.maximize_window()
+  driver.get(link)
+
+  col1 = r"div.bke1zw-1.fJrjep"
+  col2 = r"div.bke1zw-1.fKUTeK"
+  col3 = r"div.bke1zw-1.dCXEom"
+  col4 = r"div.bke1zw-1.dmqZyt"
+  col5 = r"div.bke1zw-1.hJASMb"
+
+  col = [col1, col2, col3, col4, col5]
+  img_selector = r"img.s1isp7-4.bBALuk"
+
+  for c in col:
+    divs = driver.find_elements_by_css_selector(c)
+  
+    imgs = []
+
+    print(f"Found {len(divs)} images")
+    print('Getting image src...')
+    for div in tqdm(divs):
+      action = ActionChains(driver)
+      action.move_to_element(div).perform()
+      t.sleep(0.01)
+      
+      element = div.find_elements_by_css_selector(img_selector)
+
+      while (element == None) | (element == []) | (len(element)==0):
+        t.sleep(1)
+        element = div.find_elements_by_css_selector(img_selector)
+
+      imgs.extend(element)
+
+    with open('sample.csv', 'a+') as f:
+      for img in imgs:
+        x = img.get_attribute('src')
+        x2 = img.get_attribute('alt')
+        x2 = x2.replace(":", " ")
+        x2 = x2.replace(".", " ")
+
+        f.write(f"{x2},{x}\n")
+
+  driver.quit()
 
 '''
 def get_menu(link):
--- run.py
+++ run.py
@@ -1,46 +1,49 @@
-import os
-import sys, getopt
 from tqdm import tqdm
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC   
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.select import Select
+import os, sys, getopt, shutil, glob
 import time as t
-from funcs import scrap_images, save_images, crop_image, images_path, clear_old
-import shutil
-import glob
-from PIL import Image
+#import custom functions
+from funcs import get_images, crop_and_compress, images_path
 
 def main(argv):
   start_time = t.time()
   
+  link_type = 'order'
   link = ''
-  dim = (1000,750)
-  
-  opts, args = getopt.getopt(argv, "i", longopts=["link=", "dimensions="])
+  dim = (560,420)
+  func = 0
+
+  opts, args = getopt.getopt(argv, "i", longopts=["link=", "dimensions=", "func=", "type="])
 
   for opt, arg in opts:
     if opt=="--link":
       link = arg
     elif opt=="--dimensions":
-      x = int(arg.split(',')[0])
-      y = int(arg.split(',')[1])
-
+      x = int(arg.split('x')[0])
+      y = int(arg.split('x')[1])
       dim=(x,y)
+    elif opt=="--func":
+      #1 is to scrap and save_images
+      #2 is to crop, compress and resize
+      # default is for both
+      func = int(arg)
+    elif opt=="--type":
+      link_type = arg
 
   print(f"Link: {link}\nDimensions: {dim}")
 
-  clear_old()
-  scrap_images(link)
-  save_images()
-
-  image_files = glob.glob(images_path+'//*')
-
-  print('Cropping images....')
-  for image in tqdm(image_files):
-    crop_image(image, dim)
+  if func==1:  
+    get_images(link, link_type)
+  elif func==2:
+    image_files = glob.glob(images_path+'//*')
+    #crop and compress images
+    for image in tqdm(image_files):
+      crop_and_compress(image, dim)
+  else:
+    get_images(link, link_type)
+    image_files = glob.glob(images_path+'//*')
+    #crop and compress images
+    for image in tqdm(image_files):
+      crop_and_compress(image, dim)
 
   print(f"Script executed in: {start_time - t.time()}")
 
